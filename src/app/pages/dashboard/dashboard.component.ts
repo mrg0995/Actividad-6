6ad693a310e971cbaf060ec8723b14810217a37f
Mario Ramírez <mrg0995@gmail.com>
2026-09-17 13:28:46 +0200

    feat: establecimiento de rutas

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterOutlet],
   selector: 'app-dashboard',
   styleUrl: './dashboard.component.css',
   templateUrl: './dashboard.component.html',
